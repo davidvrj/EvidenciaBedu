28d7a9850b131a9f46fd97cc160293bfc6e46ed1
davidvrj <54653409+davidvrj@users.noreply.github.com>
2020-11-26 18:13:39 -0600

    Add files via upload

---
+++ Introducción a bases de datos/sesión 1/reto1.sql
@@ -0,0 +1,10 @@
+/* Veamos la descripción de las tablas articulo, puesto y venta.
+  Recuerda que para visualizar todas las tablas del esquema puedes usar el comando:*/
+
+SHOW TABLES;
+
+DESCRIBE articulo;
+
+DESCRIBE puesto;
+
+DESCRIBE venta;
